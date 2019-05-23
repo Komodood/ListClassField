07f8db142caa32d94dd027918a4e89bb23ec9411
Komodood <36847935+Komodood@users.noreply.github.com>
2019-05-23 16:31:02 +0500

    Крыкую с непонимания в чем проблема

---
+++ ClassField/WebClassField/Controllers/UploadController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ClassField;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using WebClassField.Models;
+using WebClassField.Models.DataAccessPostgreSqlProvider;
+
+namespace WebClassField.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DoUpload(IFormFile file)
+        {
+            using (var stream = file.OpenReadStream())
+            {
+                var xs = new XmlSerializer(typeof(TableOfResult));
+                var Table = (TableOfResult)xs.Deserialize(stream);
+
+
+                using (var db = new WebClassFielddbContext())
+                {
+                    var dbs = new DbTableOfResult()
+                    {
+                        Competitions = Table.Competitions,
+                        CategoryCompetition = Table.CategoryCompetition,
+                        AgeСategory = Table.AgeСategory,
+                        DateCompetition = Table.DateCompetition,
+                        CityofComp = Table.CityofComp,
+                    };
+                    dbs.ListCompetitors = new List<DbCompetitor>();
+                    foreach (var competitor in Table.ListCompetitors)
+                    {
+                        dbs.ListCompetitors.Add(new DbCompetitor()
+                        {
+                            Name = competitor.Name,
+                            Score1 = competitor.Score1,
+                            Score2 = competitor.Score2,
+                            AllScore = competitor.AllScore,
+                        });
+                    }
+
+                    db.TableOfResult.Add(dbs);
+                    db.SaveChanges();
+                }
+
+
+                return View(Table);
+            }
+        }
+
+        //public ActionResult Image(int id)
+        //{
+        //    using (var db = new WebClassFielddbContext())
+        //    {
+        //        return base.File(db.TableOfResult.Find(id).Photo, "image/jpeg");
+        //    }
+        //}
+
+        public ActionResult List()
+        {
+            List<DbTableOfResult> list;
+            using (var db = new WebClassFielddbContext())
+            {
+                list = db.TableOfResult.Include(s => s.ListCompetitors).ToList();
+            }
+
+            return View(list);
+        }
+
+
+        public ActionResult Print(int id)
+        {
+            using (var db = new WebClassFielddbContext())
+            {
+                var Table = db.TableOfResult.Include(s1 => s1.ListCompetitors).First(s1 => s1.Id == id);
+                IWorkbook workbook =
+                    new XSSFWorkbook(System.IO.File.OpenRead("template.xlsx"));
+
+                var sheet = workbook.GetSheetAt(0);
+
+                sheet.GetRow(1).Cells[1].SetCellValue(Table.Competitions);
+
+                for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+                {
+                    IRow row = sheet.GetRow(i);
+                    if (row == null) continue;
+                    if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                    var lastCellNum = row.LastCellNum;
+                    for (int j = row.FirstCellNum; j < lastCellNum; j++)
+                    {
+                        var cell = row.GetCell(j);
+                        if (cell != null)
+                        {
+                            if (cell.StringCellValue == "$PropRow")
+                            {
+                                cell.SetCellValue("Дата соревнований");
+                                cell = row.GetCell(j + 1) ?? row.CreateCell(j + 1);
+                                cell.SetCellValue(Table.DateCompetition);
+                                cell.CellStyle.DataFormat = 14;
+
+                                row = sheet.CopyRow(i, i + 1);
+                                i++;
+                                cell = row.GetCell(j) ?? row.CreateCell(j);
+                                cell.SetCellValue("Категорий соревнований");
+                                row.CreateCell(j + 1).SetCellValue(Table.CategoryCompetition); row = sheet.CreateRow(i++);
+
+                                break;
+                            }
+
+                            if (cell.StringCellValue == "$TEST")
+                            {
+                                foreach (var competitor in Table.ListCompetitors)
+                                {
+                                    row = sheet.GetRow(i);
+                                    cell = row.GetCell(j);
+                                    cell.SetCellValue(competitor.Name);
+                                    cell = row.GetCell(j + 1) ?? row.CreateCell(j + 1);
+                                    cell.SetCellValue(competitor.Score1);
+                                    cell = row.GetCell(j + 2) ?? row.CreateCell(j + 2);
+                                    cell.SetCellValue(competitor.Score2);
+                                    cell = row.GetCell(j + 3) ?? row.CreateCell(j + 3);
+                                    cell.SetCellValue(competitor.AllScore);
+                                    if (competitor != Table.ListCompetitors.Last())
+                                        row = sheet.CopyRow(i, i + 1);
+                                    i++;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                var ms = new MemoryStream();
+                workbook.Write(ms);
+
+                ms.Position = 0;
+
+                return base.File(ms, "application/octet-stream", "Table" + id + ".xlsx");
+            }
+        }
+    }
+}
---
+++ ClassField/WebClassField/Models/DatabaseModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+
+namespace WebClassField.Models
+{
+    namespace DataAccessPostgreSqlProvider
+    {
+        public class WebClassFielddbContext : DbContext
+        {
+            // >dotnet ef migration add testMigration in AspNet5MultipleProject
+            public WebClassFielddbContext()
+            {
+                Database.EnsureCreated();
+            }
+
+            public WebClassFielddbContext(DbContextOptions<WebClassFielddbContext> options) : base(options)
+            {
+            }
+
+            public DbSet<DbTableOfResult> TableOfResult { get; set; }
+
+            public static string ConnectionString { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseNpgsql(WebClassFielddbContext.ConnectionString);
+
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        public class DbTableOfResult
+        {
+            [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int Id { get; set; }
+
+            /// <summary>
+            /// Название соревнований
+            /// </summary>
+            public string Competitions { get; set; }
+            /// <summary>
+            /// Название категории соревнований
+            /// </summary>
+            public string CategoryCompetition { get; set; }
+            /// <summary>
+            /// Возрастная категория
+            /// </summary>
+            public string AgeСategory { get; set; }
+            /// <summary>
+            /// Список участников
+            /// </summary>
+            public List<DbCompetitor> ListCompetitors { get; set; }
+            /// <summary>
+            /// Дата проведения
+            /// </summary>
+            public DateTime DateCompetition { get; set; }
+            /// <summary>
+            /// Название блоков жюри, которые учавствовали в оценивании
+            /// </summary>
+            public List<DbBlockjury> NameJury { get; set; }
+            /// <summary>
+            /// Город проведения
+            /// </summary>
+            public string CityofComp { get; set; }
+        }
+        /// <summary>
+        /// Жюри
+        /// </summary>
+        public class DbBlockjury
+        {
+            [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int Id { get; set; }
+
+            public int TableOfResultId { get; set; }
+            [ForeignKey("TableOfResultId")]
+            public virtual DbTableOfResult TableOfResult { get; set; }
+
+            /// <summary>
+            /// Имена членов жюри
+            /// </summary>
+            public List<string> ListJury { get; set; }
+            /// <summary>
+            /// Название блока Жюри
+            /// </summary>
+            public string NameJury { get; set; }
+            /// <summary>
+            /// Фотография Главного члена жюри
+            /// </summary>
+            public byte[] Photo { get; set; }
+        }
+
+        public class DbCompetitor
+        {
+            [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int Id { get; set; }
+
+            public int TableOfResultId { get; set; }
+            [ForeignKey("TableOfResultId")]
+            public virtual DbTableOfResult TableOfResult { get; set; }
+            /// <summary>
+            /// Имя участника
+            /// </summary>
+            public string Name { get; set; }
+            /// <summary>
+            /// Категория оценивания
+            /// </summary>
+            ///public CategoryScore CategoryScore { get; set; }
+            /// <summary>
+            /// Оценка за категорию
+            /// </summary>
+            public double Score1 { get; set; }
+            public double Score2 { get; set; }
+            /// <summary>
+            /// Общий балл нескольких категорий
+            /// </summary>
+            public double AllScore { get; set; }
+            /// <summary>
+            /// Фотография участника
+            /// </summary>
+            public byte[] Photo { get; set; }
+            /// <summary>
+            /// Название блоков жюри, которые учавствовали в оценивании по определенным категориям
+            /// </summary>
+            public string NameJury { get; set; }
+        }
+
+    }
+}
